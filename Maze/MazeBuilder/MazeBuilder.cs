69a8331041bdebb9b207f408a419b7a625c3d2aa
Shanis <Shanis394042@mail.ru>
2017-01-29 14:43:21 +0300

    Add maze builder class

---
+++ Maze/MazeBuilder/MazeBuilder.cs
@@ -0,0 +1,25 @@
+﻿using MazeCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeBuilder
+{
+    public delegate void BuildingAlgorithm(MazeMap map);
+
+    public class MazeBuilder
+    {
+        public BuildingAlgorithm Algorithm { get; set; }
+
+        public MazeMap BuildMaze(UInt32 width, UInt32 height)
+        {
+            var map = new MazeMap(width, height);
+
+            Algorithm?.Invoke(map);
+
+            return map;
+        }
+    }
+}
